e8b02747f6c4115e6efd3e1abd6d640ed22449b8
Aashish-Guragain <aashishgura69@gmail.com>
2026-02-09 13:50:41 -0800

    first commit

---
+++ controller/userController.js
@@ -0,0 +1,15 @@
+import user from "../model/userModel";
+const User = require("../model/userModel")
+
+const register = async (req, res) => {
+
+}
+
+
+const login = async (req, res) => {
+
+}
+modile.exports={
+    register,
+    login
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Hello, World!');
+
+
+// today we will study about MVC clean architectutre and publish to github
+
+// at first, we will learn about userside...
